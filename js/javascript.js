8af35b822f9a9a9c1214061590cf48dab8687972
niteshsharma5 <58598347+niteshsharma5@users.noreply.github.com>
2020-07-29 12:15:35 +0530

    Update javascript.js

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,15 @@
-doucument.getElementsByTagName()
+doucument.write("hey there!");
+var x=244;
+function imageEnlarge(elementId) {
+	elementId.style.width="430px";
+	elementId.style.height="auto";
+	elementId.style.margin="2px";
+	elementId.style.border="solid blue";
+}
+function backToNormal(elementId) {
+	elementId.style.width="380px";
+	elementId.style.height="auto";
+	elementId.style.margin="15px";
+	elementId.style.border="solid green";
+	
+}
